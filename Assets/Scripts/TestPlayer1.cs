404e68b5425fa4217b2815b5f82d115d5addca03
CWorld <cworld0@qq.com>
2024-04-21 22:50:19 +0800

    feat(learn): :fire: day2

---
+++ Assets/Scripts/TestPlayer1.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer1 : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    float _walkSpeed;
+    float _inputHorizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+        _walkSpeed = 5.5f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _inputHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if (_inputHorizontal != 0)
+        {
+            _rb.AddForce(new Vector2(_inputHorizontal * _walkSpeed, 0f));
+        }
+    }
+}
