c255b1f290b7759146bdab789ba8e5f1ff7ec339
CWorld <cworld0@qq.com>
2024-04-21 23:10:48 +0800

    feat(learn): :sparkles: day4

---
+++ Assets/Scripts/TestPlayer3.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer3 : MonoBehaviour
+{
+    private int _playerHealth = 100;
+    // a easier way to declare getter & setter
+    public int PlayerSpeed { get; set; }
+
+    // get & set method define
+    public int PlayerHealth
+    {
+        get
+        {
+            return _playerHealth;
+        }
+
+        // if no setter, the set action is not allowed
+        set
+        {
+            if (value > 100)
+            {
+                Debug.Log("Over the maximum value detected.");
+                return;
+            };
+            _playerHealth = value;
+        }
+    }
+
+    public string DisplayHealthPercentage
+    {
+        get
+        {
+            string health = _playerHealth.ToString() + "%";
+            return health;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerHealth += 50;
+        Debug.Log(DisplayHealthPercentage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
